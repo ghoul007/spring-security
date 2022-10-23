211b3e96735002c797da606865cf5f146a7dafca
ahmed <ahmed.elghoul@infor.com>
2022-10-23 23:28:12 +0100

    added app configuration and login endpoint

---
+++ modules/backend/src/main/java/com/ghoul/springsecurityangular/rest/AuthenticationController.java
@@ -0,0 +1,35 @@
+package com.ghoul.springsecurityangular.rest;
+
+import com.ghoul.springsecurityangular.dto.ResponseDTO;
+import com.ghoul.springsecurityangular.dto.UserDTO;
+import com.ghoul.springsecurityangular.session.InMemorySessionRegistration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class AuthenticationController {
+  @Autowired
+  public AuthenticationManager manager;
+  @Autowired
+  public InMemorySessionRegistration sessionRegistry;
+
+  @PostMapping("/login")
+  public ResponseEntity<ResponseDTO> login(@RequestBody UserDTO user) {
+    manager.authenticate(
+      new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword())
+    );
+
+    final String sessionId = sessionRegistry.registerSession(user.getUsername());
+    ResponseDTO response = new ResponseDTO();
+    response.setSessionId(sessionId);
+
+    return ResponseEntity.ok(response);
+  }
+}
